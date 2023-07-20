9a5c303982b5cfc3c2326264892f4255e6658a71
Collincehals <129850364+Collincehals@users.noreply.github.com>
2023-07-20 23:04:13 +0000

    swiper added

--- static/script.js
+++ static/script.js
@@ -5,3 +5,18 @@ menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
 }
+
+var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+    });
+  
