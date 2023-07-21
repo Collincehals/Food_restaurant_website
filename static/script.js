4862dbc383c6c0dcf1bb5b1c280b7baabf615543
Collincehals <129850364+Collincehals@users.noreply.github.com>
2023-07-21 11:26:47 +0000

    review section added and styled with Swiper.

--- static/script.js
+++ static/script.js
@@ -19,4 +19,31 @@ var swiper = new Swiper(".mySwiper", {
       },
       loop: true,
     });
-  
+
+var swiper = new Swiper(".review-container", {
+      spaceBetween: 20,
+      centeredSlides: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+      breakpoints: {
+        0:{
+          slidesPerView: 1,
+        },
+         640:{
+          slidesPerView: 2,
+        },
+         768:{
+          slidesPerView: 2,
+        },
+         1024:{
+          slidesPerView: 3,
+        },
+      },
+    });
