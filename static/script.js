672b49ae59ea937658719a01da29e050a4ae2b84
Collincehals <129850364+Collincehals@users.noreply.github.com>
2023-07-22 22:22:55 +0000

    window.scroll js added to navigate through active links. Also added is the page loader.

--- static/script.js
+++ static/script.js
@@ -6,6 +6,32 @@ menu.onclick = () =>{
   navbar.classList.toggle('active');
 }
 
+
+
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header .navbar a');
+
+window.onscroll = () =>{
+  
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset  = sec.offsetTop-150;
+    let id  = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header .navbar a[href*= '+id+']').classList.add('active');
+      });
+    };
+  });
+}
+
+
 let searchForm = document.querySelector('.search-form');
 let searchicon = document.querySelector('#search-icon');
 
@@ -13,6 +39,11 @@ searchicon.onclick=() => {
   searchForm.classList.toggle('active');
 }
 
+let closebtn = document.querySelector('#close-btn');
+
+closebtn.onclick=() => {
+  searchForm.classList.remove('active');
+}
 
 
 
@@ -57,3 +88,14 @@ var swiper = new Swiper(".review-container", {
         },
       },
     });
+
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 500);
+}
+
+window.onload = fadeOut;
